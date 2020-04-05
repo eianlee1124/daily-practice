1032e10f0e1cead11406fb7ba48a3609a3c11b3b
eian <eianlee1124@gmail.com>
2020-04-05 21:12:04 +0900

    [Python] 1,2,3 더하기

---
+++ BOJ/9095.py
@@ -0,0 +1,15 @@
+from inputs import *
+sys.stdin = init('9095')
+
+def add(n):
+    if n <= 3:
+        if n == 3:
+            return n + 1
+        return n
+    return add(n-1) + add(n-2) + add(n-3)
+
+test = int(input())
+
+for _ in range(test):
+    answer = add(int(input()))
+    print(answer)
