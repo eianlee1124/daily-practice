e80fc5e236e96e25d5e08a16320e50b0cf7ca031
eian <eianlee1124@gmail.com>
2020-04-05 21:02:27 +0900

    [Python] 피보나치함수

---
+++ BOJ/1003.py
@@ -0,0 +1,22 @@
+from inputs.defines import *
+sys.stdin = open(init('1003'))
+
+def fib(n):
+    cache = (
+        (1, 0),
+        (0, 1),
+        (1, 1)
+    )
+    
+    if n < 3:
+        return cache[n]
+    
+    a, b = 0, 1
+    for _ in range(n-1):
+        a, b = b, a + b
+    return a, b
+
+test = int(input())
+for _ in range(test):
+    n = int(input())
+    print(*fib(n))
